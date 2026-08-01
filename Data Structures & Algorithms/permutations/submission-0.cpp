2a0615a234d18135fb1162b0fb48395b902f14c2
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-08-01 18:15:29 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+vector<vector<int>>ans;
+void solve (vector<int>&nums,int i){
+    if (i==nums.size()-1){
+        ans.push_back(nums);
+        return ;
+    }
+    for (int j=i;nums.size()>j;j++){
+        swap(nums[i],nums[j]);
+        solve(nums,i+1);
+        swap(nums[i],nums[j]);
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums,0);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
